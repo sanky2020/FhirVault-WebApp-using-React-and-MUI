96c37e245dcf2d0ed9f3c670483e18624cbdead6
sanky2020 <sankyk2020@gmail.com>
2022-03-03 12:25:46 +0530

    created industries page

--- src/pages/industries.js
+++ src/pages/industries.js
@@ -1,6 +1,38 @@
-import { Typography } from "@material-ui/core";
+import React from "react";
+import "../styling/routingStyle.css";
 
 function industries() {
-  return <Typography>Industries page</Typography>;
+  return <>
+  <section>
+    <div className="bg-image bg-image-industries">
+      <div className="desc-header">
+        <h2>
+IT Solutions<br/> for Your Industry</h2>
+      </div>
+    </div>
+  </section>
+  <section className="desc-container">
+  <div className="desc">
+    <h3>Agriculture</h3>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+  </div>
+  <div className="desc">
+    <h3>Banking & Financial Services</h3>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+  </div>
+  <div className="desc">
+    <h3>Education</h3>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+  </div>
+  <div className="desc">
+    <h3>Energy & Utilities</h3>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+  </div>
+  <div className="desc">
+    <h3>Government</h3>
+    <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+  </div>
+  </section>
+</>;
 }
 export default industries;
