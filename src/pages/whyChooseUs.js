d9c62bcbec6bba923f5d0952bdd5a17bb59360a9
sanky2020 <sankyk2020@gmail.com>
2022-03-03 12:26:00 +0530

    created why choose us page

--- src/pages/whyChooseUs.js
+++ src/pages/whyChooseUs.js
@@ -1,6 +1,37 @@
-import { Typography } from "@material-ui/core";
+import React from "react";
+import "../styling/routingStyle.css";
 
 function whyChooseUs() {
-  return <Typography>why-choose-us us page</Typography>;
+  return <>
+    <section>
+      <div className="bg-image bg-image-why-chooseUs">
+        <div className="desc-header">
+          <h2>Why Choose<br/> ITG. Consulting?</h2>
+        </div>
+      </div>
+    </section>
+    <section className="desc-container">
+    <div className="desc">
+      <h3>01.</h3>
+      <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    </div>
+    <div className="desc">
+      <h3>02.</h3>
+      <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    </div>
+    <div className="desc">
+      <h3>03.</h3>
+      <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    </div>
+    <div className="desc">
+      <h3>04.</h3>
+      <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    </div>
+    <div className="desc">
+      <h3>05.</h3>
+      <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I’m a great place for you to tell a story and let your users know a little more about you.</p>
+    </div>
+    </section>
+  </>;
 }
 export default whyChooseUs;
